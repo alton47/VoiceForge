9b2f780ef0013d4c3d7df28402946b181019e9fb
Allan <altonallan47@gmail.com>
2026-03-26 15:42:06 +0300

    feat: Initialize the signIn & Organization middlewares
    Co-Authored-By: justepaix <201043863+justepaix@users.noreply.github.com>

---
+++ src/proxy.ts
@@ -0,0 +1,6 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+const isOrgSelectionRoute = createRouteMatcher(["/org-selection(.*)"]);
